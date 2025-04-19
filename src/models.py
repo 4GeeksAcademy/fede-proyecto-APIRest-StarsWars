b77cc657f0446e3d30eadb705e23126b8cc19055
Federico ferreyra <128550575+fedepaino@users.noreply.github.com>
2025-04-19 11:17:48 +0000

    relaciones añadidas atraves de favoritos

--- src/models.py
+++ src/models.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
@@ -17,3 +17,52 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class People(db.Model):
+    __tablename__ = 'people'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(120), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(120), nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(20), nullable=False)
+    height: Mapped[str] = mapped_column(String(20), nullable=False)
+    mass: Mapped[str] = mapped_column(String(20), nullable=False)
+    skin_color: Mapped[str] = mapped_column(String(20), nullable=False)
+    homeworld: Mapped[str] = mapped_column(String(40), nullable=False)
+    url: Mapped[str] = mapped_column(String(100), nullable=False)
+
+
+class Planets(db.Model):
+    __tablename__ = 'planets'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    diameter: Mapped[str] = mapped_column(String(120), nullable=False)
+    rotation_period: Mapped[str] = mapped_column(String(120), nullable=False)
+    orbital_period: Mapped[str] = mapped_column(String(120), nullable=False)
+    gravity: Mapped[str] = mapped_column(String(120), nullable=False)
+    population: Mapped[str] = mapped_column(String(120), nullable=False)
+    climate: Mapped[str] = mapped_column(String(120), nullable=False)
+    terrain: Mapped[str] = mapped_column(String(120), nullable=False)
+    surface_water: Mapped[str] = mapped_column(String(120), nullable=False)
+    
+
+class Films(db.Model):
+    __tablename__ = 'films'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(120), nullable=False)
+    episode_id: Mapped[int] = mapped_column(nullable=False)
+    opening_crowl: Mapped[str] = mapped_column(String(120), nullable=False)
+    director: Mapped[str] = mapped_column(String(120), nullable=False)
+    producer: Mapped[str] = mapped_column(String(120), nullable=False)
+    
+    
+
+
+class Favorites(db.Model):
+    __tablename__ = 'favorites'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'), nullable=False)
+    people_id: Mapped[int] = mapped_column(ForeignKey('people.id'), nullable=True)
+    planets_id: Mapped[int] = mapped_column(ForeignKey('planets.id'), nullable=True)
+    films_id: Mapped[int] = mapped_column(ForeignKey('films.id'), nullable=True)
